34b0c12dca6c2506ea06b37384bc332ee0e343f1
Sebastian Meckovski <Sebastian.Meckovski@streets-heaver.com>
2022-10-16 21:36:30 +0100

    few fixes

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,13 @@
 module.exports = {
-  "stories": [
-    "../src/Stories/**/*.stories.mdx",
-    "../src/Stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    'storybook-addon-sass-postcss'
+    "storybook-addon-sass-postcss",
   ],
-  "framework": "@storybook/react"
-}
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+};
