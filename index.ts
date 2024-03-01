f02ccc20108a693df11315a6dc2fd81b5e8d5b95
Sebastian Meckovski <sebastian.meckovski@gmail.com>
2024-03-01 23:15:49 +0000

    components tweaked

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export * from './src/components/SebComponent/SebComponent'
 export * from './src/components/BezierDrawer/BezierDrawer'
 export * from './src/components/Switch/Swtich'
+export * from './src/components/Checkbox/Checkbox'
--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -7,7 +7,7 @@ export const Checkbox = ({ ...rest }: props) => {
   return (
     <>
       <label className="checkbox-container">
-        <input type="checkbox" {...rest}/>
+        <input type="checkbox" {...rest} />
         <span className="checkbox-container-checkmark">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -18,7 +18,7 @@ export const Checkbox = ({ ...rest }: props) => {
             <path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z" />
           </svg>
         </span>
-        <span className="checkbox-container-shadow" />
+        <div className="checkbox-container-shadow"></div>
       </label>
     </>
   );
